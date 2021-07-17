fb068b9b58abdc778b1e76eeb33b95a0e2c7e929
ElusiveEric <1436326318@qq.com>
2021-07-17 08:39:02 +0800

    设置App图标

--- app/src/main/java/com/csu/freightbook/activities/HomepageActivity.java
+++ app/src/main/java/com/csu/freightbook/activities/HomepageActivity.java
@@ -3,7 +3,6 @@ package com.csu.freightbook.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
